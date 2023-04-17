3d9cd3a5a3ca2acfee16571143b8c38750ce9a5a
fer-peralta <fernandoemanuelperalta@gmail.com>
2023-04-17 19:11:51 -0300

    minor update

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -46,7 +46,7 @@ export const updateUser = async (req, res) => {
 export const deleteUser = async (req, res) => {
     try {
         const response = await UserService.deleteLogicUser(req.params.id)
-        res.status(200).json({ data: response })
+        res.status(200).json({ data: response }) 
     } catch (error) {
         res.status(400).json({ message: `There was an error deleting the user: ${error}`, error: error, section: "controller" })
     }
