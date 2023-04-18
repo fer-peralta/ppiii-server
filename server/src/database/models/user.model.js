488491a6f9d08d5c209ce8da38b07a291d8a0a96
fer-peralta <fernandoemanuelperalta@gmail.com>
2023-04-17 23:18:34 -0300

    added new structure to the model of user

--- server/src/database/models/user.model.js
+++ server/src/database/models/user.model.js
@@ -5,9 +5,10 @@ const userCollection = "users"
 mongoose.set('strictQuery', true)
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
+    email: { type: String, required: true },
     password: { type: String, required: true },
     name: { type: String, required: true },
+    surname: { type: String, required: true },
     adress: { type: String, required: true },
     age: { type: Number, required: true },
     phone: { type: String, required: true },
