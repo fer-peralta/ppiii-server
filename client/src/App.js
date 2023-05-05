ca9a7823d9523515b9ee4c80dba6a877a27acdee
romerojulian1700 <romerojulian1700@gmail.com>
2023-05-05 20:38:14 -0300

    practicando login con react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Login from "./components/Login/Login.js";
 import Register from "./components/Register/register";
 import Profile from "./components/UserProfile/USerProfile";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LOGIN, PROFILE, LOGOUT, REGISTER } from "../src/config/path"
 
 
 const queryClient = new QueryClient();
@@ -19,9 +20,11 @@ function App() {
       <QueryClientProvider client={queryClient}>
 
         <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/profile' element={<Profile />} />
+          <Route path={LOGIN} element={<Login />} />
+          <Route path={REGISTER} element={<Register />} />
+          <Route path={PROFILE} element={<Profile />} />
+          <Route path={LOGOUT} element={<Profile />} />
+
           <Route path='*' element={<h1>NOT FOUND !</h1>} />
         </Routes>
         <ReactQueryDevtools initialIsOpen={false} />
