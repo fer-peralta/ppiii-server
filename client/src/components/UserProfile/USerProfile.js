015d7fd290417fb45dfebd93f4020540cd19aa15
romerojulian1700 <85079805+romerojulian1700@users.noreply.github.com>
2023-05-07 03:09:33 -0300

    agregue boton cerra sesion en profile

--- client/src/components/UserProfile/USerProfile.js
+++ client/src/components/UserProfile/USerProfile.js
@@ -1,44 +1,57 @@
+import { useEffect } from 'react'
 import './UserProfile.css'
 import { useQuery } from 'react-query'
+import { redirect } from 'react-router-dom'
 
 const Profile = () => {
-  const url1 = 'http://localhost:8080/api/session/profile'
 
-  const token = JSON.stringify(localStorage.getItem('token'))
+    const handleLogOut = async () => {
 
-  console.log(token)
-
-  const options = {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
+        window.localStorage.removeItem('token');
+        window.location.href = 'http://localhost:3000/';
     }
-  }
+    // useEffect(() => {
+    //     handleLogOut()
+    // }, []);
 
-  const getUsers = async () => {
-    const response = await fetch(url1, options)
-    return response.json()
-  }
-  const { data, status } = useQuery('users', getUsers)
 
-  if (status === 'loading') {
-    return <p>Recuperando los users...</p>
-  }
+    const url1 = 'http://localhost:8080/api/session/profile'
 
-  if (status === 'error') {
-    return <p>Error</p>
-  }
+    const token = JSON.stringify(localStorage.getItem('token'))
 
-  return (
-    <div className='user'>
-      <div className='right'>
-        <p>Bienvenido {data.User.email}</p>
-        {/* <Link to='/' className='submitR'>Cerrar sesion</Link> */}
-      </div>
-    </div>
-  )
+    console.log(token)
+
+    const options = {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${token}`,
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const getUsers = async () => {
+        const response = await fetch(url1, options)
+        return response.json()
+    }
+    const { data, status } = useQuery('users', getUsers)
+
+    if (status === 'loading') {
+        return <p>Recuperando los users...</p>
+    }
+
+    if (status === 'error') {
+        return <p>Error</p>
+    }
+
+    return (
+        <div className='user'>
+            <div className='right'>
+                <p>Bienvenido {data.User.email}</p>
+                <button onClick={handleLogOut} className='submitR'>Cerrar sesion</button>
+            </div>
+        </div>
+    )
 }
 
 export default Profile
