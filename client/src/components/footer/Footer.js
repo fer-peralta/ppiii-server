ddbd2a5b6bf7430227d15dc3a36364b97b5d6ed0
romerojulian1700 <85079805+romerojulian1700@users.noreply.github.com>
2023-06-09 22:04:48 -0300

    Agregue footer , boton anotarse ( Falta que funcione ) y no pude mostrar subscripciones

---
+++ client/src/components/footer/Footer.js
@@ -0,0 +1,31 @@
+import './Footer.css'
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+
+
+    return (
+        <>
+            <footer className="footer">
+                <div className="social">
+                    <Link className='whatsapp' to="https://facebook.com" target="_blank"></Link>
+                    <Link className='linkedin' to="https://linkedin.com" target="_blank"></Link>
+                    <Link className='github' to="https://github.com" target="_blank"></Link>
+                </div>
+                <p className="footer_copyright">Copyright &copy; Todos los derechos reservados</p>
+
+            </footer>
+        </>
+    )
+}
+
+export default Footer
+
+
+
+
+
+
+
+
+
