787f7e98de526211905c0f16349db77817cf7064
Fernando Peralta <91640564+fer-peralta@users.noreply.github.com>
2023-10-20 22:57:30 -0300

    secure

--- server/src/services/emails/email.config.js
+++ server/src/services/emails/email.config.js
@@ -6,18 +6,13 @@ const passwordAdmin = config.ADMIN_GMAIL_PASSWORD
 
 export const transporterEmail = createTransport({
   host: 'smtp.gmail.com',
-  port: 587,
+  port: 465,
   auth: {
     user: emailAdmin,
     pass: passwordAdmin
   },
-  secure: false,
+  secure: true,
   tls: {
     rejectUnauthorized: false
   }
 })
-
-console.log(transporterEmail)
-if (transporterEmail) {
-  console.log('se armo', emailAdmin, passwordAdmin)
-}
