c0d35a4daadb096367c3e261f43a9e4b19881e8f
romerojulian1700 <romerojulian1700@gmail.com>
2023-10-23 17:33:52 -0300

    Navbar responsive

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,11 +2,14 @@ import { Link, useLocation } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import Logo from '../../assets/images/logo_white_large.png'
 import { conditionalNavList } from './Navbar.conditionalNav'
-import { NotLoggedNav } from './Navbar.notLoggedNav'
+import { NavLink } from 'react-router-dom'
+//import { NotLoggedNav } from './Navbar.notLoggedNav'
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   const location = useLocation()
   const [isLogged, setIsLogged] = useState(false)
+
 
   const token = JSON.stringify(localStorage.getItem('token'))
 
@@ -25,9 +28,49 @@ const Navbar = () => {
       location.pathname === '/register'
     ) {
       if (!isLogged) {
-        return NotLoggedNav()
+        return (<div className='navList'>
+          <div className="menu" onClick={() => setMenuOpen(!menuOpen)} >
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+
+          <ul className={menuOpen ? "open" : ""}>
+            <li>
+              <NavLink to="/home" className='nav_buttons'> Ir a home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/login" className='nav_buttons' id='profile'>
+                Login</NavLink>
+            </li>
+            <li>
+              <NavLink to='/register' className='nav_buttons'>
+                Registro</NavLink>
+            </li>
+          </ul>
+        </div>)
       } else {
-        return conditionalNavList()
+        return (
+          <div className='navList'>
+            <Link to='/home' className='nav_buttons'>
+              Home
+            </Link>
+            <Link to='/mentories' className='nav_buttons'>
+              Inicio
+            </Link>
+            <Link to='/profile' className='nav_buttons' id='profile'>
+              Perfil
+            </Link>
+            <Link to='/mentories/create' className='nav_buttons'>
+              Crear mentoría
+            </Link>
+            <Link to='/mentories/own' className='nav_buttons'>
+              Mis mentorías
+            </Link>
+            <Link to='/subscriptions' className='nav_buttons'>
+              Mis inscripciones
+            </Link>
+          </div>)
       }
     } else {
       return conditionalNavList()
